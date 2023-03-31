047310af6070326fed03e8e91ae02b5d14dc0ffd
RaGJ1206 <rkj1206@gmail.com>
2023-03-31 09:49:05 +0900

    인텔리J 세번째 커밋 서버연결(DB설정) 성공

--- test/src/main/java/com/suwon/ezen/test/controller.java
+++ test/src/main/java/com/suwon/ezen/test/controller.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/test/*")
 public class controller {
-
+    // 서버 연결 코드 작성
     @GetMapping("/hello")
     public void hello(){
         System.out.println("hello intel.");
